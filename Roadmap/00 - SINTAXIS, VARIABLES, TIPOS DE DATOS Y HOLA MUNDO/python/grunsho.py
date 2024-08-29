b397880daf82df6bc6a2cb641da3290f2e5dd2ab
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-08-29 17:11:42 -0400

    #000 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/grunsho.py
@@ -0,0 +1,30 @@
+# PYTHON
+# "https://www.python.org/"
+
+
+# Este es un comentario simple
+
+'''
+Este es un comentario multilínea
+o de bloque.
+'''
+
+"""
+Este también es un comentario 
+multilínea o de bloque.
+"""
+
+# Variables
+variable = "Valor de la variable"
+
+# Constantes (en Python no hay constantes predefinidas)
+CONSTANTE = "Valor de la constante"
+
+# Tipos de datos primitivos
+my_int = 42                 # integer
+my_float = 3.14             # float
+my_bool = True              # bool
+my_string = "Hola, mundo!"  # str 
+
+# Imprimir mensaje por terminal
+print("¡Hola, Python!")
